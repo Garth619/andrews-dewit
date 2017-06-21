bc8fa84cb4a5a0beadc76ff723494d1a6f8af9de
Garrett Cullen <garrettcullen@Garretts-iMac.local>
2017-06-21 15:07:21 -0700

    Push

--- wp-content/themes/dewitt/sidebar.php
+++ wp-content/themes/dewitt/sidebar.php
@@ -5,6 +5,14 @@
 		
 		<span class="large_header">Get legal help today</span><!-- large_header -->
 		
+		<span class="sidebar_info_required">Information Required*</span><!-- sidebar_info -->
+		
+		<div class="inner_form">
+			
+			<?php gravity_form(2, false, false, false, '', true, 12); ?>
+			
+		</div><!-- inner_form -->
+		
 	</div><!-- inner_form_wrapper -->
     <div id="sidebar">
     	<?php dynamic_sidebar( 'blog' ); ?>
