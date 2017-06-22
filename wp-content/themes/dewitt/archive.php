ff8c189a8784accd50868a50a2a84e8857e792d6
Garrett Cullen <garrettcullen@Garretts-iMac.local>
2017-06-22 10:19:53 -0700

    push

--- wp-content/themes/dewitt/archive.php
+++ wp-content/themes/dewitt/archive.php
@@ -1,14 +1,18 @@
 <?php get_header();?>
 
 <div id="primary">
-    <div id="posts">
+     <div id="inner_posts">
     	<?php wp_reset_query(); if (have_posts()) : while (have_posts()) : the_post(); ?>
-			<article class="entry post">
+			
+			
+			<div class="mysingle_post">
+				
 				<h2 class="entry-title">
 					<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
 						<?php the_title(); ?>
 					</a>
 				</h2>
+				
 				<div class="entry-meta">On behalf of DeWitt Algorri & Algorri Lawyers, A Professional Corporation<br />
 				Posted in <?php global $post;
 				$categories = get_the_category($post->ID);
@@ -16,12 +20,17 @@
 				echo '<a href="'.$cat_link.'">'.$categories[0]->cat_name.'</a>'
 				?> on <?php the_time('l, F j, Y'); ?>.
 				</div>
+				
 				<div class="entry-content"><?php echo get_the_excerpt(); ?></div>
 				<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>" class="button">Read Full Post</a>
-			</article>
+			
+			
+			</div><!-- mysingle_post -->
 		<?php endwhile; else: ?>
 		<?php endif; ?>
-    </div> 
+    
+    </div><!-- mysingle_post -->
+    
     <nav class="posts-nav">
 		<ul>
 			<li class="prev-link">
@@ -32,11 +41,8 @@
 			</li>
 		</ul>
 	</nav>
-</div>
-<div id="secondary">
-    <div id="sidebar">
-    	<?php dynamic_sidebar( 'blog' ); ?>
-    </div>
-</div>
+</div><!-- primary -->
+
+<?php include('sidebar-blog.php');?>
 
 <?php get_footer();?>
--- wp-content/themes/dewitt/single.php
+++ wp-content/themes/dewitt/single.php
@@ -1,27 +1,41 @@
 <?php get_header();?>
 
 <div id="primary">
+	<div id="inner_posts">
 	<?php wp_reset_query(); if (have_posts()) : while (have_posts()) : the_post(); ?>
-		<article class="entry post">
-			<h2 class="entry-title"><?php the_title(); ?></h2>
+		
+		
+		
+		
+		<div class="mysingle_post">
+		
+			
+			<h2><?php the_title(); ?></h2>
+			
 			<div class="entry-meta">On behalf of DeWitt Algorri & Algorri Lawyers, A Professional Corporation<br />
 				Posted in <?php global $post;
 				$categories = get_the_category($post->ID);
 				$cat_link = get_category_link($category[0]->cat_ID);
 				echo '<a href="'.$cat_link.'">'.$categories[0]->cat_name.'</a>'
 				?> on <?php the_time('l, F j, Y'); ?>.
-				</div>
 			</div>
+			
+			
 			<div class="entry-content"><?php the_content(); ?></div>
-		</article>
+		
+			
+				
+			</div><!-- mysingle_post -->
+	
+	
+	
+	
 	<?php endwhile; else: ?>
 	<?php endif; ?>
-</div>
-<div id="secondary">
-    <div id="sidebar">
-    	<?php dynamic_sidebar( 'blog' ); ?>
-    </div>
-</div>
+ </div> <!-- inner posts -->
+
+</div><!-- primary -->
+<?php include('sidebar-blog.php');?>
 
 
 <?php get_footer();?>
