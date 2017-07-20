3caad721b56ac673d9e69216c2619991ab07392a
Garrett Cullen <garrettcullen@Garretts-iMac.local>
2017-07-20 10:44:59 -0700

    Push

---
+++ wp-content/themes/dewitt/page-news.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Template Name: In the News
+ */
+
+get_header(); ?>
+
+<div id="primary">
+    
+    
+<h1 id="page-title"><?php the_title(); ?></h1>
+
+ <div id="page-content">
+
+<div class="news_wrapper">
+	
+	<div class="news_single">
+		
+		<h2>2020</h2>
+		
+		<p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		
+	</div><!-- news_single -->
+	
+	<div class="news_single">
+		
+		<h2>2020</h2>
+		
+		<span class="new_subtitle"><strong>Sub Title</strong></span><!-- new_subtitle -->
+		
+		<p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		
+	</div><!-- news_single -->
+	
+	<div class="news_single">
+		
+		<h2>2020</h2>
+		
+		<p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		
+	</div><!-- news_single -->
+	
+	<div class="news_single">
+		
+		<h2>2020</h2>
+		
+		<p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		
+	</div><!-- news_single -->
+	
+</div><!-- news_wrapper -->
+
+ </div><!-- page-content -->
+
+
+   
+</div>
+<?php include('sidebar.php');?>
+    
+<?php get_footer(); ?>
+
+
+
+
+
+
+
